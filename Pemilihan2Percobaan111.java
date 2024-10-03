60a9700e216a46d0c54cfb7deae6d42dc014c315
haf5th <hafiflearning@gmail.com>
2024-10-03 16:22:42 +0700

    Pertanyaan

--- Pemilihan2Percobaan111.java
+++ Pemilihan2Percobaan111.java
@@ -10,10 +10,16 @@ public class Pemilihan2Percobaan111 {
 
         if ((tahun % 4) == 0) {
             if ((tahun % 100) != 0) {
+                if ((tahun % 400) != 0) {
                     System.out.println("Tahun Kabisat");
+                } else {
+                    System.out.println("Bukan Tahun Kabisat");
+                }
+            } else {
+                System.out.println("Bukan Tahun Kabisat");
+            }
         } else {
             System.out.println("Bukan Tahun Kabisat");
         }
     }
-}
 }
